530229f5c86b4c6d53738892f5ba58e8b0091c7d
Matthijs Nieuwboer <matthijsnieuwboer@gmail.com>
2015-02-27 14:57:54 +0100

    Add file reader to snippet generator, NEEDS TO BE FIXED

--- src/main/java/org/sg/SnippetGenerator.java
+++ src/main/java/org/sg/SnippetGenerator.java
@@ -1,14 +1,30 @@
 package org.sg;
 
-import org.apache.commons.cli.Options;
-
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 /**
  * Created by zyixc @ matthijsnieuwboer@gmail.com on 2/5/2015.
  */
 public class SnippetGenerator {
     public static void main(String[] args) throws IOException {
-        Options options = new Cli(args).getOptions();
+        Cli cli = new Cli(args);
+        HashMap<Integer,String> words = readFile(cli.getDocumentName());
+    }
+
+    private static HashMap<Integer,String> readFile(String documentName){
+        HashMap<Integer,String> words = null;
+        try {
+            words = Files.lines(Paths.get(documentName))
+                .flatMap(line -> Arrays.stream(line.split(" .")))
+                .collect(Collectors.toMap());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words;
     }
 }
